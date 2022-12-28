7fe29a782643d01a742f570573cfa1b9a8b3bba1
vntero <shin.antero@gmail.com>
2022-12-28 18:44:40 +0100

    changed db port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv/config");
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 8080;
 const cors = require("cors");
 const router = express.Router()
 
