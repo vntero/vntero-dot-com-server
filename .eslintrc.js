1cef92111e78a2ccaaad9d5df75dc40f233a8707
vntero <shin.antero@gmail.com>
2023-01-20 10:50:23 +0000

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+    'security',
+    'sonarjs',
+    // "standard",
+    // "prettier",
+  ],
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:security/recommended',
+    'plugin:sonarjs/recommended',
+    // "plugin:prettier/recommended",
+    // "prettier/@typescript-eslint",
+    // "prettier/standard",
+  ],
+  rules: {
+    '@typescript-eslint/no-use-before-define': ['off', { functions: false }],
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/namespace': ['off'],
+    // "@typescript-eslint/indent": ["error", 2],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      functions: 'never',
+    }],
+    'no-useless-constructor': ['off'],
+    '@typescript-eslint/no-useless-constructor': ['error'],
+    '@typescript-eslint/no-parameter-properties': [
+      'error',
+      { allows: ['readonly', 'private readonly'] },
+    ],
+    'no-trailing-spaces': ['error', { ignoreComments: true }],
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    es6: true,
+    'jest/globals': true,
+  },
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // -------------------------------- REQUIREMENTS ---------------------------------
-        // ℹ️ Gets access to environment variables/settings
-        // https://www.npmjs.com/package/dotenv
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
 require("dotenv/config");
 
 const express = require('express')
@@ -23,79 +23,79 @@ db.once('open', () => console.log('Connected to database'))
 
 // ------------------------------------- CORS ----------------------------------------
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.ORIGIN || 3000,
-    })
-  );
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN || 3000,
+  })
+);
 
 // ---------------------------------- MOONGOOSE -------------------------------------
-        // 0. connect to our database, mongodb
-        // LOCALHOST CONNECTION: ------> mongoose.connect('mongodb://127.0.0.1:27017/Messages');
+// 0. connect to our database, mongodb
+// LOCALHOST CONNECTION: ------> mongoose.connect('mongodb://127.0.0.1:27017/Messages');
 mongoose.connect(MONGODB_URI);
-        
-        // 1. Define your schema 
-let MessageSchema = new mongoose.Schema ({
-    name: String,
-    email: String, 
-    message: String
+
+// 1. Define your schema 
+let MessageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String
 })
 
-        // 2. Define your model
-let MessageModel = mongoose.model ('MessageModel', MessageSchema)
+// 2. Define your model
+let MessageModel = mongoose.model('MessageModel', MessageSchema)
 
 
 // ----------------------------------- EXPRESS ------------------------------------
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-        // this is the home page - root directory
+// this is the home page - root directory
 app.get('/', (req, res) => {
-    res.send('Back-end is up and running! This is the root directory.')
-  })
+  res.send('Back-end is up and running! This is the root directory.')
+})
 
 // --------------------------------------- BACKEND TESTING ROUTES ------------------------------------------------------
-        // this is simply another directory (one where we'll handle our form by grabbing the html file)
+// this is simply another directory (one where we'll handle our form by grabbing the html file)
 app.get('/d4l', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 })
-        // GRAB the form submission into the variable, POST it to our DB and redirect to the home page
+// GRAB the form submission into the variable, POST it to our DB and redirect to the home page
 app.post('/d4l', (req, res) => {
-    let messageOne = new MessageModel({
-        name: req.body.name,
-        email: req.body.email,
-        message: req.body.message
-    })
-    messageOne.save()
-    res.send('Successfully saved in our DB!')
+  let messageOne = new MessageModel({
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
+  })
+  messageOne.save()
+  res.send('Successfully saved in our DB!')
 })
 
 // --------------------------------------- RETRIEVE ALL MESSAGES SAVED IN OUR DB ------------------------------------------------
 
 app.get('/retriever', (req, res) => {
-        res.send('Here we shall see all messages saved in our db:')
-        
+  res.send('Here we shall see all messages saved in our db:')
+
 })
 
 // ---------------- BACKEND TESTING ROUTES WORK SMOOTHLY, ROUTES LOAD AND CREATE ENTRIES IN THE DB ------------------------------
 
 // will handle all POST requests to http:localhost:5005/new-message
-app.post('/new-message', (req, res) => {  
-    const {name, email, message} = req.body;
-    
-    MessageModel.create({name, email, message})
-          .then((response) => {
-               res.status(200).json(response)
-          })
-          .catch((err) => {
-               res.status(500).json({
-                    error: 'Something went wrong',
-                    message: err
-               })
-          }) 
+app.post('/new-message', (req, res) => {
+  const { name, email, message } = req.body;
+
+  MessageModel.create({ name, email, message })
+    .then((response) => {
+      res.status(200).json(response)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: 'Something went wrong',
+        message: err
+      })
+    })
 })
 
 app.listen(PORT, () => {
-    console.log(`SERVER RUNNING at http://localhost:${PORT}`)
+  console.log(`SERVER RUNNING at http://localhost:${PORT}`)
 })
 
