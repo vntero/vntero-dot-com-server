8896cc5f285b4cbb96b2a84e1e6e2e5719ed293c
vntero <shin.antero@gmail.com>
2023-09-02 22:55:47 +0200

    removes attachmentes

--- src/mailer.js
+++ src/mailer.js
@@ -16,43 +16,7 @@ const mailOptions = {
   subject: 'Hello from Nodemailer',
   text: 'This is a test email sent from Nodemailer.',
   html: html,
-  attachments: [
-    {
-      filename: 'aeler_logo.png',
-      path: '../assets/aeler_logo.png',
-      cid: 'aelerlogo',
-    },
-    {
-      filename: 'facebook_ico.png',
-      path: '../assets/facebook_ico.png',
-      cid: 'facebooklogo',
-    },
-    {
-      filename: 'instagram_ico.png',
-      path: '../assets/instagram_ico.png',
-      cid: 'instagramlogo',
-    },
-    {
-      filename: 'linkedin_ico.png',
-      path: '../assets/linkedin_ico.png',
-      cid: 'linkedinlogo',
-    },
-    {
-      filename: 'twitter_ico.png',
-      path: '../assets/twitter_ico.png',
-      cid: 'twitterlogo',
-    },
-    {
-      filename: 'url_ico.png',
-      path: '../assets/url_ico.png',
-      cid: 'urllogo',
-    },
-    {
-      filename: 'youtube_ico.png',
-      path: '../assets/youtube_ico.png',
-      cid: 'youtubelogo',
-    },
-  ],
+  attachments: [],
 }
 
 transporter.sendMail(mailOptions, (error, info) => {
